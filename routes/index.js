5b9a03825ef2c01f247c4899366e3d4d7793f098
David <thedaviddraw@gmail.com>
2022-10-29 17:41:24 -0500

    tercer_endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const{createAcount} = require ('../controllers');
 const{addfundsAcount} = require ('../controllers');
-
+const{orderandSell} = require ('../controllers');
 
     const router = express.Router();
 
     router.post('/acount', createAcount);
     router.put('/acount/:id', addfundsAcount);
+    router.post('/acount/:id/order', orderandSell);
     
 
     module.exports = {
